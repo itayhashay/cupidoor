ae6dd3c60e769e64c2713aac651d273e99a18947
itayhashay <itay0806@gmail.com>
2023-05-01 23:40:49 +0300

    question crud

---
+++ server/server/service/question.service.js
@@ -0,0 +1,53 @@
+const Question = require('../model/question.model');
+ 
+const createQuestion = async (questionData) => {
+  try {
+    const question = new Question(questionData);
+    return await question.save();
+  } catch (err) {
+    throw new Error('Error creating question: ' + err.message);
+  }
+}
+ const getQuestions = async () => {
+  try {
+    return await Question.find();
+  } catch (err) {
+    throw new Error('Error getting questions: ' + err.message);
+  }
+}
+ 
+const getQuestion = async (id) => {
+  try {
+    return await Question.findById(id);
+  } catch (err) {
+    throw new Error('Error getting question: ' + err.message);
+  }
+}
+
+const updateQuestion= async (id, questionData) => {
+  try {
+    const question = await Question.findById(id);
+    if (questionData.question != null) {
+      question.question = questionData.question;
+    }
+    return await question.save();
+  } catch (err) {
+    throw new Error('Error updating question: ' + err.message);
+  }
+}
+
+const deleteQuestion = async (id) => {
+  try {
+    return await Question.findByIdAndDelete(id);
+  } catch (err) {
+    throw new Error('Error deleting question: ' + err.message);
+  }
+}
+
+module.exports = {
+  createQuestion,
+  getQuestions,
+  getQuestion,
+  updateQuestion,
+  deleteQuestion
+};
