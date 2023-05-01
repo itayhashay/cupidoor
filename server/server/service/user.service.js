0b9f80178abf485e16de859b4be025e9117edf95
itayhashay <itay0806@gmail.com>
2023-05-01 22:43:36 +0300

    user crud

---
+++ server/server/service/user.service.js
@@ -0,0 +1,72 @@
+const User = require('../model/user.model');
+
+const createUser = async (userData) => {
+  try {
+    const user = new User(userData);
+    return await user.save();
+  } catch (err) {
+    throw new Error('Error creating user: ' + err.message);
+  }
+}
+
+const getUsers = async () => {
+  try {
+    return await User.find();
+  } catch (err) {
+    throw new Error('Error getting users: ' + err.message);
+  }
+}
+
+const getUser = async (id) => {
+  try {
+    return await User.findById(id);
+  } catch (err) {
+    throw new Error('Error getting user: ' + err.message);
+  }
+}
+
+const updateUser = async (id, userData) => {
+  try {
+    const user = await User.findById(id);
+    if (userData.name != null) {
+      user.name = userData.name;
+    }
+    if (userData.email != null) {
+      user.email = userData.email;
+    }
+    if (userData.firstName != null) {
+      user.firstName = userData.firstName;
+    }
+    if (userData.lastName != null) {
+      user.lastName = userData.lastName;
+    }
+    if (userData.age != null) {
+      user.age = userData.age;
+    }
+    if (userData.password != null) {
+      user.password = userData.password;
+    }
+    if (userData.role != null) {
+      user.role = userData.role;
+    }
+    return await user.save();
+  } catch (err) {
+    throw new Error('Error updating user: ' + err.message);
+  }
+}
+
+const deleteUser = async (id) => {
+  try {
+    return await await User.findByIdAndDelete(id);
+  } catch (err) {
+    throw new Error('Error deleting user: ' + err.message);
+  }
+}
+
+module.exports = {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
+};
