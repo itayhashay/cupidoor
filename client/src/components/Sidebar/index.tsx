79e39009ec978e155b1b85d9ddfddbdabaa66958
itay <itay@carwiz.co.il>
2023-05-10 01:05:10 +0300

    Add life style filters

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -4,43 +4,38 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TuneIcon from '@mui/icons-material/Tune';
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import { Drawer, DrawerHeader, FilterIcon } from "./styles";
-import { BasicFilters } from "../../utils/filters";
+import { BasicFilters, Filter, LifeStyleFilters } from "../../utils/filters";
 import { Box, Tooltip, Typography } from "@mui/material";
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
 
   const ToogleDrawer = () => {
     setOpen(!open);
   };
 
-  return (
-    <>
-      <CssBaseline />
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader sx={{direction: "rtl",
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "center",
-                          height: "auto"}}>
-          <IconButton onClick={ToogleDrawer}>
-            {!open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-          <Typography variant="h5" sx={{ marginLeft: "5px", visibility: open ? "visible" : "hidden"}}>
-            Filters
-          </Typography>
-        </DrawerHeader>
-        <List>
-        {BasicFilters.map((item) => (
+  const renderFilters = (filters: Filter[]) => {
+    return <>
+            {filters.map((item) => (
               <ListItem key={item.id} disablePadding sx={{ display: "block" }}>
                   <Box
                     sx={{
                       minHeight: 48,
-                      justifyContent: open ? "initial" : "center",
+                      justifyContent: "initial",
                       display: "flex",
                       px: 2.5,
                       alignItems: "center"
@@ -56,7 +51,7 @@ const Sidebar = () => {
                         <FilterIcon src={item.icon} />
                       </ListItemIcon>
                     </Tooltip>
-                    <Box sx={{ visibility: open ? "visible" : "hidden", padding: "0 5px"}} >
+                    <Box sx={{ padding: "0 5px", display: "flex", justifyContent: 'center'}} >
                     {item.component}
                     </Box>
 
@@ -64,6 +59,48 @@ const Sidebar = () => {
                   <Divider sx={{margin: "0 auto 8px auto", width: "90%"}}/>
               </ListItem>
             ))}
+    </>
+  }
+
+  return (
+    <>
+      <CssBaseline />
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader sx={{direction: "rtl",
+                          display: "flex",
+                          justifyContent: "space-between",
+                          alignItems: "center",
+                          height: "auto"}}>
+          <IconButton onClick={ToogleDrawer}>
+            {!open ? <TuneIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+          <Typography variant="h5" sx={{ marginLeft: "5px", visibility: open ? "visible" : "hidden", fontWeight: "500"}}>
+            Filters
+          </Typography>
+        </DrawerHeader>
+        <List sx={{ visibility: open ? "visible" : "hidden" }}>
+        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} sx={{ margin: "0px !important" }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            Basic Filters
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{padding: '0'}}>
+          {renderFilters(BasicFilters)}
+        </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')} sx={{ margin: "0px !important" }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Life Style Filters</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{padding: '0'}}>
+        {renderFilters(LifeStyleFilters)}
+        </AccordionDetails>
+      </Accordion>
         </List>
       </Drawer>
     </>
--- client/src/utils/filters.tsx
+++ client/src/utils/filters.tsx
@@ -6,6 +6,12 @@ import BalconyIcon from "../icons/aparment/balcony.png";
 import PriceIcon from "../icons/aparment/shekel.png";
 import PrecentIcon from "../icons/aparment/percentage.png";
 
+import PetIcon from "../icons/lifeStyle/pet.png";
+import SmokeIcon from "../icons/lifeStyle/smoke.png";
+import RoomatesIcon from "../icons/lifeStyle/roomates.png";
+import SalaryIcon from "../icons/lifeStyle/salary.png";
+import RelationsIcon from "../icons/lifeStyle/relationship.png";
+
 import PrecentsSlider from "../components/Filters/PrecentsRangeSlider";
 import PriceRangeSlider from "../components/Filters/PriceRangeSlider";
 import RoomsRangeSlider from "../components/Filters/RoomsRangeSlider";
@@ -13,9 +19,13 @@ import SquareMeterRangeSlider from "../components/Filters/SquareMeterRangeSlider
 import FloorRangeSlider from "../components/Filters/FloorRangeSlider";
 import BalconyRangeSlider from "../components/Filters/BalconyRangeSlider";
 import ParkingRangeSlider from "../components/Filters/ParkingRangeSlider";
+import Switch from "@mui/material/Switch";
 
+export type Filter = {
+  id: number, displayName: string, icon: string, component?: JSX.Element
+}
 
-export const BasicFilters: { id: number, displayName: string, icon: string, component?: JSX.Element}[] = [
+export const BasicFilters: Filter[] = [
   {
     id: 1,
     displayName: "Minimum Match",
@@ -59,4 +69,38 @@ export const BasicFilters: { id: number, displayName: string, icon: string, comp
     component: <ParkingRangeSlider />
   },
 ];
+
+export const LifeStyleFilters: { id: number, displayName: string, icon: string, component?: JSX.Element}[] = [
+  {
+    id: 1,
+    displayName: "Pets",
+    icon: PetIcon,
+    component: <Switch defaultChecked />
+  },
+  {
+    id: 2,
+    displayName: "Smoke",
+    icon: SmokeIcon,
+    component: <Switch defaultChecked />
+  },
+  {
+    id: 3,
+    displayName: "Roomates",
+    icon: RoomatesIcon,
+    component: <Switch defaultChecked />
+  },
+  {
+    id: 4,
+    displayName: "Income",
+    icon: SalaryIcon,
+    component: <Switch defaultChecked />
+  },
+  {
+    id: 5,
+    displayName: "Landlord",
+    icon: RelationsIcon,
+    component: <Switch defaultChecked />
+  },
+];
+
 
