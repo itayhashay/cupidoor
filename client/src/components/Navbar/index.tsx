f8ccc0f0b374fbf09f2bf7bb128dedf64254fac4
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2023-06-07 02:45:14 +0300

    good navbar and start of landing

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
-import { AppBar, Box, Toolbar, Button } from "@mui/material";
+import { AppBar, Box, Toolbar, Button, Grid, MenuItem } from "@mui/material";
 import { Avatar, Typography } from "@mui/material";
-import { DividerLine, LogoImg, UserSection, linkStyles } from "./styles";
+import { LogoImg, UserSection, linkStyles } from "./styles";
 import Logo from "../../icons/logo.png";
 import UserImg from "../../icons/user.jpeg";
 import { Link } from "react-router-dom";
@@ -11,36 +11,102 @@ export const Navbar = () => {
   const { user, setUser } = useAuth();
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{ flexGrow: 1, position: "sticky", top: "0", zIndex: "100" }}>
+      <AppBar sx={{ position: "static" }}>
         <Toolbar>
-          <Link to={`/home/${USER_ROUTES.ALL_APARTMENTS}`}>
-            <LogoImg src={Logo} alt="logo" />
-          </Link>
-          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-            Cupidoor
-          </Typography>
-          <UserSection>
-            {user ? (
-              <>
-                <Typography
-                  variant="h6"
-                  sx={{ marginRight: "16px" }}
-                >{`Hello ${user.firstName}`}</Typography>
-                <Link to={"/user/personal-info"}>
-                  <Avatar alt="" src={UserImg} />
+          <Grid container>
+            <Grid item xs={3}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Link to={`/home/${USER_ROUTES.ALL_APARTMENTS}`}>
+                  <LogoImg src={Logo} alt="logo" />
                 </Link>
-              </>
-            ) : (
-              <>
-                <Button sx={linkStyles}>
-                  <Link to={"/signIn"} >
-                    Sign In
-                  </Link>
-                </Button>
-              </>
-            )}
-          </UserSection>
+                <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                  Cupidoor
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid
+              item
+              xs={5}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MenuItem>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    About
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    How it works
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Contact
+                  </Typography>
+                </MenuItem>
+              </Box>
+            </Grid>
+            <Grid
+              item
+              xs={4}
+              sx={{
+                display: "flex",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "end",
+                }}
+              >
+                <UserSection>
+                  {user ? (
+                    <>
+                      <Typography
+                        variant="h6"
+                        sx={{ marginRight: "16px" }}
+                      >{`Hello ${user.firstName}`}</Typography>
+                      <Link to={"/user/personal-info"}>
+                        <Avatar alt="" src={UserImg} />
+                      </Link>
+                    </>
+                  ) : (
+                    <>
+                      <Button sx={linkStyles}>
+                        <Typography
+                          component={Link}
+                          to={"/signIn"}
+                          sx={{ color: "white" }}
+                        >
+                          Sign In
+                        </Typography>
+                      </Button>
+                    </>
+                  )}
+                </UserSection>
+              </Box>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </Box>
