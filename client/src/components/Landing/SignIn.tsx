abcef5f179b59ac54c077e94ccc6a3550916f4cc
Afik Nof <afiknof9@gmail.com>
2023-06-18 21:09:02 +0300

    Sign in loader

--- client/src/components/Landing/SignIn.tsx
+++ client/src/components/Landing/SignIn.tsx
@@ -1,13 +1,13 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { Button, CssBaseline, TextField, Box } from "@mui/material";
-import { Typography, Container, Link, Grid, Paper } from "@mui/material";
-import LockIcon from "@mui/icons-material/Lock";
-import { loginScheme, CustomHelperText } from "./AuthHelpers";
-import { Formik } from "formik";
-import "./SignIn.css";
-import { AuthContextType, useAuth } from "../../context/AuthContext";
-import { useSnackbar } from "../../context/SnackbarContext";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Button, CssBaseline, TextField, Box, CircularProgress } from '@mui/material';
+import { Typography, Container, Link, Grid, Paper } from '@mui/material';
+import LockIcon from '@mui/icons-material/Lock';
+import { loginScheme, CustomHelperText } from './AuthHelpers';
+import { Formik } from 'formik';
+import './SignIn.css';
+import { AuthContextType, useAuth } from '../../context/AuthContext';
+import { useSnackbar } from '../../context/SnackbarContext';
 
 interface SignInPageProps {
   onTogglePage: () => void;
@@ -16,126 +16,121 @@ interface SignInPageProps {
 const SignInPage = ({ onTogglePage }: SignInPageProps) => {
   const navigate = useNavigate();
   const location = useLocation();
-  const [isGoodLogin, setIsGoodLogin] = useState<boolean>(false);
-  const [loginTimeoutId, setLoginTimeoutId] = useState<
-    NodeJS.Timeout | undefined
-  >();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { snackBarState, setSnackBarState } = useSnackbar();
   const { signInUser } = useAuth() as AuthContextType;
 
-  const onSubmitHandler = async (values: {
-    email: string;
-    password: string;
-  }) => {
+  const onSubmitHandler = async (values: { email: string; password: string }) => {
     const userData = {
       email: values.email,
       password: values.password,
     };
+    setIsLoading(true);
+
     const response: any = await signInUser(userData.email, userData.password);
-    console.log(response);
+    setIsLoading(false);
     if (response.success) {
       setSnackBarState({
-        severity: "success",
-        message: "Welcome Back!",
+        severity: 'success',
+        message: 'Welcome Back!',
         show: true,
       });
-      navigate(location.state ? location.state.redirect : "/questions"); // isAllAnswered ? redirect /home
+      navigate(location.state ? location.state.redirect : '/questions'); // isAllAnswered ? redirect /home
     } else {
-      alert(response.data);
+      setSnackBarState({
+        severity: 'error',
+        message: response.error,
+        show: true,
+      });
     }
   };
-
-  if (isGoodLogin) {
-    return <div>Logged good! redirecting you...</div>;
-  }
 
   return (
     <>
       <Formik
         validationSchema={loginScheme}
-        initialValues={{ email: "", password: "" }}
+        initialValues={{ email: '', password: '' }}
         onSubmit={(event) => onSubmitHandler(event)}
       >
         {({ values, errors, touched, handleChange, handleSubmit }) => (
-          <Container component="main" maxWidth="lg">
+          <Container component='main' maxWidth='lg'>
             <CssBaseline />
             <Paper elevation={3} sx={{ width: 1152, height: 704 }}>
-              <Grid container height={"100%"}>
+              <Grid container height={'100%'}>
                 <Grid item xs={6} padding={3}>
                   <Grid container>
                     <Grid item xs={12}>
                       <Box
-                        display={"flex"}
-                        justifyContent={"space-between"}
+                        display={'flex'}
+                        justifyContent={'space-between'}
                         width={100}
-                        alignItems={"center"}
+                        alignItems={'center'}
                       >
-                        <img src="/favicon.png" width={90}></img>
+                        <img src='/favicon.png' width={90}></img>
                         <Typography
-                          fontWeight={"bold"}
-                          fontFamily={"Roboto sans-serif"}
-                          fontSize={"1.3rem"}
+                          fontWeight={'bold'}
+                          fontFamily={'Roboto sans-serif'}
+                          fontSize={'1.3rem'}
                         >
                           CupiDoor
                         </Typography>
                       </Box>
                     </Grid>
                     <Grid item xs={12}>
-                      <Box display={"flex"} justifyContent={"center"}>
-                        <Typography variant="h4" fontWeight={"bold"}>
+                      <Box display={'flex'} justifyContent={'center'}>
+                        <Typography variant='h4' fontWeight={'bold'}>
                           Welcome Back!
                         </Typography>
                       </Box>
-                      <Box display={"flex"} justifyContent={"center"}>
-                        <Typography variant="subtitle1" color={"#A9A9A9"}>
+                      <Box display={'flex'} justifyContent={'center'}>
+                        <Typography variant='subtitle1' color={'#A9A9A9'}>
                           Let's Find Your Next Tenant Or Apartment
                         </Typography>
                       </Box>
                     </Grid>
                     <Grid item xs={12}>
-                      <Box
-                        component={"form"}
-                        noValidate
-                        onSubmit={handleSubmit}
-                      >
+                      <Box component={'form'} noValidate onSubmit={handleSubmit}>
                         <TextField
-                          type="email"
-                          name="email"
-                          id="email"
+                          type='email'
+                          name='email'
+                          id='email'
                           value={values.email}
-                          margin="normal"
+                          margin='normal'
                           required
                           fullWidth
-                          label="Email Address"
-                          autoComplete="email"
+                          label='Email Address'
+                          autoComplete='email'
                           autoFocus
                           onChange={handleChange}
                         />
-                        {errors?.email && (
-                          <CustomHelperText>{errors.email}</CustomHelperText>
-                        )}
+                        {errors?.email && <CustomHelperText>{errors.email}</CustomHelperText>}
                         <TextField
-                          name="password"
-                          id="password"
-                          type="password"
-                          label="Password"
+                          name='password'
+                          id='password'
+                          type='password'
+                          label='Password'
                           value={values.password}
-                          margin="normal"
+                          margin='normal'
                           required
                           fullWidth
-                          autoComplete="current-password"
+                          autoComplete='current-password'
                           onChange={handleChange}
                         />
-                        {errors?.password && (
-                          <CustomHelperText>{errors.password}</CustomHelperText>
-                        )}
+                        {errors?.password && <CustomHelperText>{errors.password}</CustomHelperText>}
 
                         <Button
-                          type="submit"
+                          type='submit'
+                          disabled={isLoading}
+                          startIcon={
+                            isLoading ? (
+                              <CircularProgress size={14}></CircularProgress>
+                            ) : (
+                              <LockIcon></LockIcon>
+                            )
+                          }
                           fullWidth
-                          variant="contained"
+                          variant='contained'
                           sx={{ mt: 3, mb: 2 }}
-                          startIcon={<LockIcon></LockIcon>}
                         >
                           Sign In
                         </Button>
@@ -143,19 +138,15 @@ const SignInPage = ({ onTogglePage }: SignInPageProps) => {
                     </Grid>
 
                     <Grid item xs={12}>
-                      <Box
-                        display={"flex"}
-                        justifyContent={"space-between"}
-                        paddingX={3}
-                      >
-                        <Link href="#" variant="body2">
+                      <Box display={'flex'} justifyContent={'space-between'} paddingX={3}>
+                        <Link href='#' variant='body2'>
                           Forgot password?
                         </Link>
-                        <Typography variant="body2">
+                        <Typography variant='body2'>
                           Don't have an account?
                           <Link
-                            variant="body2"
-                            sx={{ px: 1, cursor: "pointer" }}
+                            variant='body2'
+                            sx={{ px: 1, cursor: 'pointer' }}
                             onClick={onTogglePage}
                           >
                             Sign Up
@@ -169,14 +160,12 @@ const SignInPage = ({ onTogglePage }: SignInPageProps) => {
                   item
                   xs={6}
                   sx={{
-                    backgroundImage: "url(/signIn.png)",
-                    backgroundRepeat: "no-repeat",
+                    backgroundImage: 'url(/signIn.png)',
+                    backgroundRepeat: 'no-repeat',
                     backgroundColor: (t) =>
-                      t.palette.mode === "light"
-                        ? t.palette.grey[50]
-                        : t.palette.grey[900],
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
+                      t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
                   }}
                 ></Grid>
               </Grid>
--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,15 +1,9 @@
-import {
-  createContext,
-  useContext,
-  useState,
-  FunctionComponent,
-  useEffect,
-} from "react";
-import { User } from "../types/user";
-import { AxiosError, AxiosResponse } from "axios";
-import { CupidAxiosError } from "../types/cupidAxiosError";
-import useAPI from "../hooks/useAPI";
-import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { createContext, useContext, useState, FunctionComponent, useEffect } from 'react';
+import { User } from '../types/user';
+import { AxiosError, AxiosResponse } from 'axios';
+import { CupidAxiosError } from '../types/cupidAxiosError';
+import useAPI from '../hooks/useAPI';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
 type Props = { children: React.ReactNode };
 
@@ -23,7 +17,7 @@ export type AuthContextType = {
   signUpUser: (user: User) => void;
   signOutUser: () => void;
   fetchUser: () => void;
-  updateUser:(newUserData:User)=>void;
+  updateUser: (newUserData: User) => void;
 };
 
 const AuthContext = createContext<AuthContextType>({
@@ -36,7 +30,7 @@ const AuthContext = createContext<AuthContextType>({
   signUpUser: () => {},
   signOutUser: () => {},
   fetchUser: () => {},
-  updateUser:()=>{}
+  updateUser: () => {},
 });
 
 export default AuthContext;
@@ -53,7 +47,7 @@ export const AuthContextProvider: FunctionComponent<Props> = ({ children }) => {
   const axiosPrivate = useAxiosPrivate();
   // Games yet not finished - storing the user in local storage, adv will be the cookie
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
+    const storedUser = localStorage.getItem('user');
     if (storedUser) {
       setUser(JSON.parse(storedUser));
     }
@@ -61,7 +55,7 @@ export const AuthContextProvider: FunctionComponent<Props> = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    const storedAccessToken = localStorage.getItem("accessToken");
+    const storedAccessToken = localStorage.getItem('accessToken');
     if (storedAccessToken) {
       setAccessToken(storedAccessToken);
     }
@@ -69,26 +63,26 @@ export const AuthContextProvider: FunctionComponent<Props> = ({ children }) => {
 
   useEffect(() => {
     if (user) {
-      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem('user', JSON.stringify(user));
     }
   }, [user]);
 
   useEffect(() => {
     if (accessToken) {
-      localStorage.setItem("accessToken", String(accessToken));
+      localStorage.setItem('accessToken', String(accessToken));
     }
   }, [accessToken]);
 
   const signInUser = async (email: string, password: string) => {
-    const response: AxiosResponse = await signIn(email, password);
-
-    if (response.status == 200) {
+    try {
+      const response: AxiosResponse = await signIn(email, password);
       const { user, accessToken }: User = response.data;
       setUser(user);
       setAccessToken(accessToken);
       return { success: true };
+    } catch (ex: any) {
+      return { success: false, error: ex.response.data.error };
     }
-    return { success: false, error: response.data };
   };
 
   const signUpUser = async (user: User) => {
@@ -104,34 +98,35 @@ export const AuthContextProvider: FunctionComponent<Props> = ({ children }) => {
 
   const signOutUser = async () => {
     try {
-      await axiosPrivate.get("/signOut");
-      localStorage.removeItem("user");
-      localStorage.removeItem("accessToken");
+      await axiosPrivate.get('/signOut');
+      localStorage.removeItem('user');
+      localStorage.removeItem('accessToken');
       setUser(null);
       setAccessToken(null);
     } catch (ex) {}
   };
 
-  const updateUser = async (newUserData:User)=>{
-    try{
-      const response:AxiosResponse = await axiosPrivate.put(`/user/${user?._id}`,{...user,...newUserData});
-      if(response.status === 200){
+  const updateUser = async (newUserData: User) => {
+    try {
+      const response: AxiosResponse = await axiosPrivate.put(`/user/${user?._id}`, {
+        ...user,
+        ...newUserData,
+      });
+      if (response.status === 200) {
         setUser(response.data);
       }
-    }catch(ex){
+    } catch (ex) {
       throw ex;
     }
-  }
+  };
 
   const fetchUser = async () => {
-    const response = await axiosPrivate.get("/user");
+    const response = await axiosPrivate.get('/user');
     const userData: User = response.data;
     setUser((prevState) => {
       return { ...prevState, ...userData };
     });
   };
-
-
 
   const contextData: AuthContextType = {
     user,
@@ -143,10 +138,8 @@ export const AuthContextProvider: FunctionComponent<Props> = ({ children }) => {
     signUpUser: signUpUser,
     signOutUser: signOutUser,
     fetchUser: fetchUser,
-    updateUser:updateUser
+    updateUser: updateUser,
   };
 
-  return (
-    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>;
 };
