49d1720fea28ba23198a356dca1e3269dd527c6b
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2023-08-01 22:23:15 +0300

    fix match iteration bug

--- server/server/router.js
+++ server/server/router.js
@@ -3,37 +3,29 @@ const match = require("./model/match");
 const landlords = require("./mock/landlords.json");
 const verifyToken = require("./middlewares/verifyToken");
 
-router.use("/user",[verifyToken], require("./controller/user.controller"));
-router.use("/apartment", [verifyToken],require("./controller/apartment.controller"));
-router.use("/question",[verifyToken], require("./controller/question.controller"));
+router.use("/user", [verifyToken], require("./controller/user.controller"));
+router.use(
+  "/apartment",
+  [verifyToken],
+  require("./controller/apartment.controller")
+);
+router.use(
+  "/question",
+  [verifyToken],
+  require("./controller/question.controller")
+);
 router.use("/score", require("./controller/score.controller"));
-router.use("/user-answer",[verifyToken], require("./controller/usersAnswer.controller"));
-router.use("/users-relations",[verifyToken], require("./controller/usersRelations.controller"));
+router.use(
+  "/user-answer",
+  [verifyToken],
+  require("./controller/usersAnswer.controller")
+);
+router.use(
+  "/users-relations",
+  [verifyToken],
+  require("./controller/usersRelations.controller")
+);
 router.use("/", require("./routes/auth.routes"));
-router.use("/chat",[verifyToken], require("./routes/chat.routes"));
-
-router.post("/match", (req, res, next) => {
-  const { answers, priority } = req.body;
-  let matchPercentage = [];
-  for (let landlord of landlords.landlords) {
-    const landlordData = {
-      name: landlord.name,
-      address: landlord.address,
-      floor: landlord.floor,
-      price: landlord.price,
-      parkings: landlord.parkings,
-      rooms: landlord.rooms,
-    };
-    matchPercentage.push({
-      ...landlordData,
-      match: match({ answers, priority }, landlord),
-    });
-  }
-
-  matchPercentage = matchPercentage.sort((a, b) => {
-    return a.match > b.match ? -1 : 1;
-  });
-  res.send(matchPercentage);
-});
+router.use("/chat", [verifyToken], require("./routes/chat.routes"));
 
 module.exports = router;
