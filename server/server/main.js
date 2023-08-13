0d747395a82c1c92d3a9b0b635d9996e9cc9170f
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2023-08-14 00:06:52 +0300

    styling main.js in server

--- server/server/main.js
+++ server/server/main.js
@@ -7,7 +7,7 @@ const cookieParaser = require("cookie-parser");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 
-const {initializeChat} = require("./middlewares/chat");
+const { initializeChat } = require("./middlewares/chat");
 const errorHandler = require("./middlewares/errorHandler");
 const { env } = require("process");
 const swaggerDocument = require("./swagger.json");
@@ -15,27 +15,27 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const allowedOrigins = ["http://193.106.55.150:3000","http://cupidoor.cs.colman.ac.il","http://localhost:3000","http://localhost:2308"]
+const allowedOrigins = [
+  "http://193.106.55.150:3000",
+  "http://cupidoor.cs.colman.ac.il",
+  "http://localhost:3000",
+  "http://localhost:2308",
+];
 const swaggerSpec = swaggerJsDoc(swaggerDocument);
-
-
-
-
-
 
 app.use(
   cors({
-    origin: (origin,callback)=>{
-      if(!origin || allowedOrigins.indexOf(origin) > -1){
-        callback(null,true);
-      }else{
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) > -1) {
+        callback(null, true);
+      } else {
         callback(new Error("CORS Error: Access denied for origin:" + origin));
       }
     },
     credentials: true,
   })
 );
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParaser());
 
 app.use(mongoSanitize());
@@ -43,13 +43,11 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use("/", require("./router"));
 app.use(errorHandler);
 
-
-
 const startServer = async () => {
   try {
     console.log(chalk.yellow("\nTrying to connect to mongoDB"));
     await mongoose.connect(process.env.MONGOURI);
-    
+
     console.log(chalk.green("MongoDB connected successfully"));
   } catch (ex) {
     console.error(ex.message);
