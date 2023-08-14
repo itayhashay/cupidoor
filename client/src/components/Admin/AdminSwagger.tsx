9489e573afae74e4b3073cab65c691040b69285f
AfikNof95 <afiknof9@gmail.com>
2023-08-14 16:26:39 +0300

    swagger

--- client/src/components/Admin/AdminSwagger.tsx
+++ client/src/components/Admin/AdminSwagger.tsx
@@ -4,7 +4,7 @@ import useAPI from "../../hooks/useAPI";
 const AdminSwagger = ()=>{
     const {} = useAPI();
     return (
-        <iframe src="http://localhost:2308/api-docs" width={"100%"} height={"100%"}>
+        <iframe src="http://193.106.55.149:3000/api-docs" title="swagger" width={"100%"} height={"100%"}>
 
         </iframe>
     );
